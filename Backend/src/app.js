65e61583fca1b1f05caf56858ab7b51f68b7e71f
Adhar1777 <149721236+AdharBattulwar@users.noreply.github.com>
2024-10-06 19:24:00 +0530

    check for the logout cors issue

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -3,7 +3,6 @@ import cors from "cors";
 import userRouter from "./routes/user.routes.js";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
-
 dotenv.config({
   path: "./src/config/.env",
 });
@@ -14,21 +13,19 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
-
-const whitelist = (process.env.WHITELIST_DOMAINS || "").split(",");
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true,
-};
-app.use(cors(corsOptions));
-app.options("*", cors(corsOptions));
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (process.env.WHITELIST_DOMAINS.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
 app.use("/api/v1/user", userRouter);
 
