d75b8e645777e999244ce5aadf2745b319975db3
Adhar1777 <149721236+AdharBattulwar@users.noreply.github.com>
2024-10-06 18:48:16 +0530

    fixed the env issue in Whitelist_Domain

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -2,6 +2,11 @@ import express from "express";
 import cors from "cors";
 import userRouter from "./routes/user.routes.js";
 import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+
+dotenv.config({
+  path: "./src/config/.env",
+});
 
 const app = express();
 
@@ -10,20 +15,20 @@ app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-const whitelist = process.env.WHITELIST_DOMAINS.split(",");
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (whitelist.indexOf(origin) !== -1 || !origin) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-  })
-);
+const whitelist = (process.env.WHITELIST_DOMAINS || "").split(",");
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
+};
+app.use(cors(corsOptions));
+app.options("*", cors(corsOptions));
 
 app.use("/api/v1/user", userRouter);
 
--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,16 +1,16 @@
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 
 dotenv.config({
-    path:"./src/config/.env"
-})
+  path: "./src/config/.env",
+});
 
 //Mongo connection
-import connection from "./db/index.js"
+import connection from "./db/index.js";
 connection();
 
 //app config
 import app from "./app.js";
 
-app.listen(process.env.PORT || 3000,()=>{
-    console.log(`The Server is running on port ${process.env.PORT}`)
-})
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`The Server is running on port ${process.env.PORT}`);
+});
