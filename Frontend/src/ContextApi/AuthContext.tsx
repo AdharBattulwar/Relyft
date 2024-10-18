5e3df94f1b5d18aa93b9f67a32107d422adf671e
Adhar1777 <149721236+AdharBattulwar@users.noreply.github.com>
2024-10-18 23:18:08 +0530

    Refactor AuthContext component: Add authentication functionality and user state management

---
+++ Frontend/src/ContextApi/AuthContext.tsx
@@ -0,0 +1,40 @@
+import { createContext, useState, useEffect } from "react";
+
+const AuthContext = createContext<{
+  user: object | null;
+  loginUser: (userData: object) => void;
+  logoutUser: () => void;
+}>({
+  user: null,
+  loginUser: () => {},
+  logoutUser: () => {}
+});
+
+import { ReactNode } from "react";
+
+export const AuthProvider = ({ children }: { children: ReactNode }) => {
+  const [user, setUser] = useState<object | null>(null);
+
+  const loginUser = (userData:object) => {
+    setUser(userData);
+  };
+
+  const logoutUser = () => {
+    setUser(null);
+  };
+
+  useEffect(() => {
+    const savedUser = localStorage.getItem("user");
+    if (savedUser) {
+      setUser(JSON.parse(savedUser));
+    }
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user, loginUser, logoutUser }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthContext;
