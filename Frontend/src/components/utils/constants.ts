12f121be58036eac0c6d63d34da3cc424ec8508b
Adhar Battulwar <adharbattulwar24@gmail.com>
2025-09-12 08:32:11 +0000

    hotfix: changed the server URL

--- Frontend/src/components/utils/constants.ts
+++ Frontend/src/components/utils/constants.ts
@@ -1,4 +1,4 @@
-const SERVER_URL = "https://relyft-production.up.railway.app";
+const SERVER_URL = "https://relyft.onrender.com";
 // const SERVER_URL = "http://localhost:8000";
 
 export { SERVER_URL };
